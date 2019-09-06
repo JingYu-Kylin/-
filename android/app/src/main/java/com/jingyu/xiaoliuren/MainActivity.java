24c1f85e424093f46be4cc1b20b6d34d9037434a
kylin <875840673@qq.com>
2019-09-06 11:06:21 +0800

    first commit

---
+++ android/app/src/main/java/com/jingyu/xiaoliuren/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jingyu.xiaoliuren;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
